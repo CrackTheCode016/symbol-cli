4ad35eb32fdceb3ac35a9a68db158eafc811c992
Bader <bader@Baders-MacBook-Pro-615.local>
2020-03-09 10:33:01 -0400

    added tests for resource editor

--- src/respositories/resources.editor.repository.ts
+++ src/respositories/resources.editor.repository.ts
@@ -1,14 +1,54 @@
 import * as path from 'path'
+import * as preader from 'properties-reader'
 import { TemplateConfig, NodeType } from '../models/template'
+import PropertiesReader = require('properties-reader')
+
+
+/**
+ * Resources property interface.
+ */
+export interface Property {
+    propertyName: string;
+    value: number | string | boolean;
+}
+
+/**
+ * Peer endpoint interface.
+ */
+export interface PeerEndpoint {
+    host: string;
+    port: number;
+}
+
+/**
+ * Peer metadata interface.
+ */
+export interface PeerMetadata {
+    name: string;
+    roles: string;
+}
+
+/**
+ * Resources peer interface.
+ */
+export interface Peer {
+    publicKey: string;
+    endpoint: PeerEndpoint;
+    metadata: PeerMetadata;
+}
 
 /**
  * Template resources editor repository.
  */
 export class ResourcesEditor {
 
-
     // constants for resource configuration
-    private readonly RESOURCE_NAMES: string[] = []
+
+    private readonly SHARED_CONFIG_FILE_NAMES: string[] = ['inflation', 'network', 'task', 'timesync', 'user']
+
+    private readonly PEER_CONFIG_FILE_NAMES: string[] = ['harvesting']
+
+    private readonly API_CONFIG_NAMES: string[] = ['database', 'messaging', 'pt']
 
     private readonly API_EXTENSIONS: string[] = ['filespooling', 'partialtransaction']
 
@@ -23,10 +63,9 @@ export class ResourcesEditor {
     private readonly DUAL_EXTENSIONS: string[] = this.SHARED_EXTENSIONS
         .concat(this.API_EXTENSIONS, this.PEER_EXTENSIONS)
 
+    private resourcesPath: string
     private readonly PEER_P2P = 'peers-p2p.json'
     private readonly PEER_API = 'peers-api.json'
-
-    private resourcesPath: string
 
     constructor(public readonly templateUrl: string) {
         this.resourcesPath = path.join(templateUrl, 'resources')
@@ -37,6 +76,13 @@ export class ResourcesEditor {
      * @param {string} alternativeConfigUrl - Alternative configuration file
      */
     public applyConfig(alternativeConfigUrl?: string) {
+        throw new Error('not implemented')
+    }
+
+    /**
+     * readSelectedParameter
+     */
+    public readSelectedParameter(propertyName: string, resourceName: string): Property {
         throw new Error('not implemented')
     }
 
@@ -64,7 +110,7 @@ export class ResourcesEditor {
     /**
      * changeMaxFee
      */
-    public changeMaxFee(newMaxFee: string) {
+    public changeMaxFee(newMaxFee: number) {
         throw new Error('not implemented')
     }
 
@@ -78,14 +124,21 @@ export class ResourcesEditor {
     /**
      * addNewPeer - Adds a new peer to peers.json
      */
-    public addNewPeer(ip: string, role: NodeType) {
+    public addNewPeer(publicKey: string, ip: string, role: NodeType) {
+        throw new Error('not implemented')
+    }
+
+    /**
+     * getPeers - Adds a new peer to peers.json
+     */
+    public getPeers(): Peer[] {
         throw new Error('not implemented')
     }
 
     /**
      * changeExtentionsProperties - Changes a extension set given a list of extensions.
      */
-    private changeExtentionsProperties(extensions: string[]) {
+    private changeExtentionsProperties(extensions: Property[]) {
         throw new Error('not implemented')
     }
 
@@ -115,5 +168,17 @@ export class ResourcesEditor {
     */
     private buildResourceFileUrl(name: string): string {
         return path.join(this.resourcesPath, `config-${name}.properties`)
+    }
+
+    /**
+     * changeSelectedProperty - Changes a selected property, given a properties file.
+     */
+    private changeSelectedProperties(file: string, properties: Property[]) {
+        const fileUrl = this.buildResourceFileUrl(file)
+        const loadedProperties = PropertiesReader(fileUrl)
+        for (let property of properties) {
+            loadedProperties.set(property.propertyName, property.value)
+        }
+        loadedProperties.save(fileUrl)
     }
 }
--- test/repositories/resources.editor.repository.spec.ts
+++ test/repositories/resources.editor.repository.spec.ts
@@ -1,36 +1,75 @@
 import { expect } from 'chai'
+import { ResourcesEditor } from '../../src/respositories/resources.editor.repository'
+import { NodeType } from '../../src/models/template'
 
 describe('Resource Editor', () => {
 
-    it('should create a new repository', () => {
+    let templateUrl: string
+    let alternateConfigUrl: string
 
+    before(() => {
+        templateUrl = './test/mocks/sample-template'
+        alternateConfigUrl = './test/mocks/sample-template/other-config-file.json'
+    })
+
+    it('should create a new repository', () => {
+        const editorRepository = new ResourcesEditor(templateUrl)
+        expect(editorRepository).to.not.be.equal(undefined)
     })
 
     it('should change the maxfee', () => {
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.changeMaxFee(100)
+        const maxFeeProperty = editorRepository.readSelectedParameter('node.minFeeMultiplier', 'node')
+        expect(maxFeeProperty.propertyName).to.be.equal('node.minFeeMultiplier')
+        expect(maxFeeProperty.value).to.be.equal(100)
 
     })
 
     it('should change the boot private key', () => {
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.changeBootKey('1000000000000000000000000000000000000000000000000000000000000001')
+        const bootKeyProperty = editorRepository.readSelectedParameter('account.bootPrivateKey', 'user')
+        expect(bootKeyProperty.propertyName).to.be.equal('account.bootPrivateKey')
+        expect(bootKeyProperty.value).to.be.equal('1000000000000000000000000000000000000000000000000000000000000001')
 
     })
 
     it('should change the harvester key', () => {
-
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.changeBootKey('1000000000000000000000000000000000000000000000000000000000000001')
+        const bootKeyProperty = editorRepository.readSelectedParameter('account.bootPrivateKey', 'user')
+        expect(bootKeyProperty.propertyName).to.be.equal('account.bootPrivateKey')
+        expect(bootKeyProperty.value).to.be.equal('1000000000000000000000000000000000000000000000000000000000000001')
     })
 
     it('should change the friendly name', () => {
-
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.changeHarvestingKey('2220000000000000000000000000000000000000000000000000000000000001')
+        const harvestingKeyProperty = editorRepository.readSelectedParameter('harvesting.harvesterPrivateKey', 'user')
+        expect(harvestingKeyProperty.propertyName).to.be.equal('account.bootPrivateKey')
+        expect(harvestingKeyProperty.value).to.be.equal('2220000000000000000000000000000000000000000000000000000000000001')
     })
 
     it('should add a new peer to the resources', () => {
-
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.addNewPeer('1000000000000000000000000000000000000000000000000000000000000001', 'newnodeip.com', NodeType.Peer)
+        const peers = editorRepository.getPeers()
+        expect(peers.length).to.be.equal(2)
+        expect(peers[1].publicKey).to.be.equal('1000000000000000000000000000000000000000000000000000000000000001')
     })
 
     it('should apply an alternate configuration to resources', () => {
-
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.applyConfig(alternateConfigUrl)
+        const bootKey = editorRepository.readSelectedParameter('account.bootPrivateKey', 'user')
+        expect(bootKey.value).to.be.equal('bootKeyDifferent')
     })
 
     it('should change the topology of a node', () => {
-
+        const editorRepository = new ResourcesEditor(templateUrl)
+        editorRepository.changeTopology(NodeType.Dual)
+        const nodeRoles = editorRepository.readSelectedParameter('localnode.roles', 'node')
+        expect(nodeRoles.value).to.be.equal('Peer,Api')
     })
 })
