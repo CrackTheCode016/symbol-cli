151090818a6278f774c8cdfb5c5df43cb99586e2
Bader <bader@Baders-MacBook-Pro-615.local>
2020-03-09 12:29:22 -0400

    fixed editor tests

--- test/repositories/resources.editor.repository.spec.ts
+++ test/repositories/resources.editor.repository.spec.ts
@@ -20,6 +20,7 @@ describe('Resource Editor', () => {
     it('should change the maxfee', () => {
         const editorRepository = new ResourcesEditor(templateUrl)
         editorRepository.changeMaxFee(100)
+        editorRepository.applyConfig()
         const maxFeeProperty = editorRepository.readSelectedParameter('node.minFeeMultiplier', 'node')
         expect(maxFeeProperty.propertyName).to.be.equal('node.minFeeMultiplier')
         expect(maxFeeProperty.value).to.be.equal(100)
@@ -29,6 +30,7 @@ describe('Resource Editor', () => {
     it('should change the boot private key', () => {
         const editorRepository = new ResourcesEditor(templateUrl)
         editorRepository.changeBootKey('1000000000000000000000000000000000000000000000000000000000000001')
+        editorRepository.applyConfig()
         const bootKeyProperty = editorRepository.readSelectedParameter('account.bootPrivateKey', 'user')
         expect(bootKeyProperty.propertyName).to.be.equal('account.bootPrivateKey')
         expect(bootKeyProperty.value).to.be.equal('1000000000000000000000000000000000000000000000000000000000000001')
@@ -38,6 +40,7 @@ describe('Resource Editor', () => {
     it('should change the harvester key', () => {
         const editorRepository = new ResourcesEditor(templateUrl)
         editorRepository.changeBootKey('1000000000000000000000000000000000000000000000000000000000000001')
+        editorRepository.applyConfig()
         const bootKeyProperty = editorRepository.readSelectedParameter('account.bootPrivateKey', 'user')
         expect(bootKeyProperty.propertyName).to.be.equal('account.bootPrivateKey')
         expect(bootKeyProperty.value).to.be.equal('1000000000000000000000000000000000000000000000000000000000000001')
@@ -46,6 +49,7 @@ describe('Resource Editor', () => {
     it('should change the friendly name', () => {
         const editorRepository = new ResourcesEditor(templateUrl)
         editorRepository.changeHarvestingKey('2220000000000000000000000000000000000000000000000000000000000001')
+        editorRepository.applyConfig()
         const harvestingKeyProperty = editorRepository.readSelectedParameter('harvesting.harvesterPrivateKey', 'user')
         expect(harvestingKeyProperty.propertyName).to.be.equal('account.bootPrivateKey')
         expect(harvestingKeyProperty.value).to.be.equal('2220000000000000000000000000000000000000000000000000000000000001')
@@ -53,10 +57,13 @@ describe('Resource Editor', () => {
 
     it('should add a new peer to the resources', () => {
         const editorRepository = new ResourcesEditor(templateUrl)
-        editorRepository.addNewPeer('1000000000000000000000000000000000000000000000000000000000000001', 'newnodeip.com', NodeType.Peer)
+        editorRepository
+            .addNewPeer('cool-node', '1000000000000000000000000000000000000000000000000000000000000001', 'newnodeip.com', NodeType.Peer)
         const peers = editorRepository.getPeers()
         expect(peers.length).to.be.equal(2)
         expect(peers[1].publicKey).to.be.equal('1000000000000000000000000000000000000000000000000000000000000001')
+        expect(peers[1].name).to.be.equal('cool-node')
+
     })
 
     it('should apply an alternate configuration to resources', () => {
